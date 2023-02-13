80fec3dfa22e7a296582440c27bcb0080b48ae97
emanuele-depaoli-sdg <emanuele.depaoli@sdggroup.com>
2023-02-13 17:28:42 +0000

    update other package files

--- R/zero.R
+++ R/zero.R
@@ -6,7 +6,9 @@
 #' @param x numeric
 #' @importFrom readxl read_excel
 #' @importFrom readr write_delim
-#' @importFrom dplyr %>% filter select distinct group_by summarise mutate semi_join bind_rows
+#' @importFrom dplyr %>% filter select distinct group_by ungroup summarise slice if_else reframe mutate inner_join left_join semi_join bind_rows rows_update n_distinct pull
+#' @importFrom tidyr expand_grid
+#' @importFrom tibble tibble
 #' @examples
 #' zero()
 #' @export
--- run6.R
+++ run6.R
@@ -23,14 +23,14 @@ file_mapping_entity <- 'TE_IRRBB_MAPPING_ENTITY_V2.xlsx'
 # term structure
 file_term_structure <- "curve_1y.xlsx"
 # notional - output sezione 5
-file_notional <- "notional.csv"
+file_notional <- "notional_prova.csv"
 
 #----------------- 002 SETTINGS parameters  -----------------------------------#
 
 mesi_tenor_prepayement <- 180
 prepayment <- 'SI' #SI/NO
-percentile1=0.999
-percentile2=0.960
+percentile1 <- 0.999
+percentile2 <- 0.960
 formula_delta_pv <- 'GESTIONALE' #GESTIONALE/SEGNALETICA
 storicizza_delta_pv <- 'SI' #SI/NO
 max_x <- 480
@@ -47,6 +47,13 @@ mapping_entity <- read_excel(file.path(path_in_local, file_mapping_entity),
 # caricamento term structure
 curve_1y <- read_excel(file.path(path_in_local, file_term_structure))
 
+# notional base
+notional_base <- read_delim(file.path(path_out_local, "notional_base_prova.csv"),
+                            skip = 1,
+                            delim = ";",
+                            col_names = c("COD_VALUTA_FINALE", "COD_ENTITY", "ID_MESE_MAT", "DES_SHOCK_FINALE", "VAL_NOTIONAL"),
+                            show_col_types = F)
+
 #--------------- 004 CARICAMENTO FILE OUTPUT SEZIONI PRECEDENTI ---------------#
 
 # caricamento notional
@@ -54,20 +61,59 @@ notional <- read_delim(file.path(path_out_local, file_notional),
                        skip = 1,
                        delim = ";",
                        col_names = c("COD_VALUTA_FINALE", "COD_ENTITY", "ID_MESE_MAT", "DES_SHOCK_FINALE", "VAL_NOTIONAL"),
+                       col_types = "ccdcd",
                        show_col_types = F)
+
+# shock effettivi
+shock_effettivi <- read_delim(file.path(path_out_local, file_notional),
+                              skip = 1,
+                              delim = ";",
+                              col_names = c("COD_VALUTA", "DES_SHOCK_FINALE", "ID_MESE_MAT", "VAL_SHOCK_EFFETTIVO_BPS", "VAL_SHOCK_NOMINALE_BPS"),
+
+                              show_col_types = F)
 
 
 ################################################################################
 #---------------------------- FASE DI CALCOLO-----------------------------------
 ################################################################################
 
+#---------------------- 000 DIVISIONE NOTIONAL: PREP - NO PREP ----------------#
+.notional <- do_notional_prep_noprep(.notional = notional)
+notional_prep <- .notional$notional_prep
+notional_noprep <- .notional$notional_noprep
+
 
 #---------------------- 001 CALCOLO ENTITY AGGREGATA --------------------------#
 
-notional <- do_entity_aggregata(.notional = notional,
+notional <- do_entity_aggregata(.notional_prep = notional_prep,
+                                .notional_noprep = notional_noprep,
                                 .mapping_entity = mapping_entity)
 
 #---------------------- 002 CALCOLO INTERPOLAZIONE SPLINE ---------------------#
 
 curve_1y_interpol <- do_interpolazione_spline(.curve_1y = curve_1y)
 
+#---------------------- 003 CALCOLO DISCOUNT FACTOR ---------------------------#
+
+curve_1y_interpol <- do_discount_factor(.curve_1y_interpol = curve_1y_interpol)
+
+# --------------------- 004 SELEZIONE SCENARIO SHOCK --------------------------#
+
+.selezione_scenario_shock <- do_selezione_scenario_shock(.curve_1y_interpol = curve_1y_interpol,
+                                                         .shock_effettivi = shock_effettivi,
+                                                         .prepayment = prepayment,
+                                                         .scenario_no_prepayment = scenario_no_prepayment)
+
+scenari_noprep <- .selezione_scenario_shock$scenari_noprep
+
+scenari_prep <- .selezione_scenario_shock$scenari_prep
+
+# -------------------- 005 CALCOLO DELTA PV -----------------------------------#
+
+deltaPV <- do_deltapv(.scenari_prep = scenari_prep,
+                      .scenari_noprep = scenari_noprep,
+                      .notional_prep = notional_prep,
+                      .notional_noprep = notional_noprep,
+                      .notional_base = notional_base,
+                      .curve_1y_interpol = curve_1y_interpol,
+                      .formula_delta_pv = formula_delta_pv)
