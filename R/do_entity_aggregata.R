765a8d490f3d19c20b8bf2fa8c4aeea161daf027
emanuele-depaoli-sdg <emanuele.depaoli@sdggroup.com>
2023-02-13 15:42:50 +0000

    upload do_entity_aggregata

--- R/do_entity_aggregata.R
+++ R/do_entity_aggregata.R
@@ -1,7 +1,13 @@
 #' do_entity_aggregata
 #' @description
 #' Aggiunge entity aggregata al notional.
-#' @param .notional tibble with 5 variables:
+#' @param .notional_prep tibble with 5 variables:
+#' * COD_VALUTA_FINALE chr,
+#' * COD_ENTITY chr,
+#' * ID_MESE_MAT dbl,
+#' * DES_SHOCK_FINALE dbl,
+#' * VAL_NOTIONAL dbl.
+#' #' @param .notional_noprep tibble with 5 variables:
 #' * COD_VALUTA_FINALE chr,
 #' * COD_ENTITY chr,
 #' * ID_MESE_MAT dbl,
@@ -19,30 +25,39 @@
 #' * DES_SHOCK_FINALE dbl,
 #' * VAL_NOTIONAL dbl.
 #' @export
-do_entity_aggregata <- function(.notional, .mapping_entity) {
+do_entity_aggregata <- function(.notional_prep, .notional_noprep, .mapping_entity) {
 
-  .entity_capogruppo <- .mapping_entity %>%
+  # espansione notional_noprep
+  elenco_shock <- .notional_prep %>%
+    distinct(DES_SHOCK_FINALE) %>%
+    pull()
+
+  notional_espanso <- expand_grid(.notional_noprep, elenco_shock) %>%
+    select(COD_VALUTA_FINALE, COD_ENTITY, ID_MESE_MAT, VAL_NOTIONAL, DES_SHOCK_FINALE = elenco_shock)
+
+  notional <- bind_rows(.notional_prep, notional_espanso)
+
+  entity_capogruppo <- mapping_entity %>%
     filter(FLG_CAPOGRUPPO == 'Y') %>%
     select(COD_ENTITY) %>%
     distinct()
 
-  .totale00001 <- .notional %>%
-    group_by(DES_SHOCK_FINALE,COD_VALUTA_FINALE,ID_MESE_MAT) %>%
+  totale00001 <- notional %>%
+    group_by(DES_SHOCK_FINALE, COD_VALUTA_FINALE, ID_MESE_MAT) %>%
     summarise(VAL_NOTIONAL = sum(VAL_NOTIONAL, na.rm = TRUE), .groups = 'drop') %>%
     mutate(COD_ENTITY = "00001")
 
 
-  .totale00005 <- .notional %>%
-    semi_join(.entity_capogruppo, by = "COD_ENTITY") %>%
-    group_by(DES_SHOCK_FINALE,COD_VALUTA_FINALE,ID_MESE_MAT) %>%
-    summarise(VAL_NOTIONAL = sum(VAL_NOTIONAL, na.rm = TRUE), .groups = 'drop') %>%
+  totale00005 <- notional %>%
+    semi_join(entity_capogruppo, by="COD_ENTITY") %>%
+    group_by(DES_SHOCK_FINALE, COD_VALUTA_FINALE, ID_MESE_MAT) %>%
+    summarise(VAL_NOTIONAL = sum(VAL_NOTIONAL,na.rm = TRUE), .groups = 'drop') %>%
     mutate(COD_ENTITY = "00005")
 
-
   #accodiamo alla shift_aggregata
-  .notional <- bind_rows(.notional, .totale00001, .totale00005)
+  notional_prep <- bind_rows(notional_prep, totale00001,totale00005)
 
   # return
-  return(.notional)
+  return(notional_prep)
 
 }
