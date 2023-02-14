3e6ea82b913af3eaf09f0ffcdc171acde27be3e3
emanuele-depaoli-sdg <emanuele.depaoli@sdggroup.com>
2023-02-14 11:24:24 +0000

    upload docker

---
+++ script/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Source env
+source ./script/env.sh
+
+# docker build
+docker build  -t $IMAGE  $DEV_PATH
+
+# Run Docker locally
+# docker run  -v /data/:/data/ ecap11s5
---
+++ script/env.sh
@@ -0,0 +1,12 @@
+
+# project name and image
+PRJ=ecap11s6
+IMAGE=ecap11s6
+
+# development project path: ~/dev/ecap11s5
+DEV_PATH=~/dev/ecap11s6
+
+# version from package
+VERSION=$( grep Version: $DEV_PATH/DESCRIPTION | awk '{print $2}')
+
+
