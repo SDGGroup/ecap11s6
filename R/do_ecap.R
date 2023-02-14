3e45b97c0ec21f586779b4fd16b179d9c9d34781
emanuele-depaoli-sdg <emanuele.depaoli@sdggroup.com>
2023-02-14 10:21:42 +0000

    upload do_ecap e selezione_curve_ecap

---
+++ R/do_ecap.R
@@ -0,0 +1,76 @@
+#' do_ecap.R
+#' @description
+#' tba
+#' @param .deltapv a tibble object with 7 variables:
+#' * ID_YEAR dbl,
+#' * COD_VALUTA chr,
+#' * ID_SCEN dbl,
+#' * DES_SHOCK_FINALE chr,
+#' * COD_ENTITY chr,
+#' * DELTA_PV dbl,
+#' * DES_PREPAYMENT chr.
+#' @param .mapping_entity
+#' * COD_ENTITY chr,
+#' * DES_ENTITY chr,
+#' * COD_NU_TDB chr,
+#' * COD_BU_RND chr,
+#' * FLG_CAPOGRUPPO chr,
+#' @param .quantiles vector of quantiles
+#' @param .prepayment chr.
+#' @return a tibble object with 9 variables:
+#' * ID_YEAR dbl,
+#' * COD_VALUTA chr,
+#' * COD_ENTITY chr,
+#' * ECAP dbl,
+#' * VAL_PERCENTILE dbl,
+#' * ID_SCEN dbl,
+#' * DES_SHOCK_FINALE chr,
+#' * DES_PREPAYMENT chr,
+#' * COD_RIPARTIZIONE dbl.
+#' @export
+
+do_ecap <- function(.deltapv, .mapping_entity, .quantiles){
+
+  ECAP <- .do_ecap_arricchito(.deltapv, .quantiles)
+
+  ECAP_2 <- ECAP %>%
+    group_by(ID_YEAR, COD_VALUTA, VAL_PERCENTILE, DES_PREPAYMENT) %>%
+    mutate(ECAP_00001 = ECAP[COD_ENTITY == "00001"]) %>%
+    filter(!COD_ENTITY %in% c("00001", "00005")) %>%
+    mutate(peso = ECAP/sum(ECAP),
+           ECAP = ECAP_00001*peso,
+           COD_RIPARTIZIONE = 2) %>%
+    ungroup() %>%
+    select(ID_YEAR, COD_VALUTA, COD_ENTITY, ECAP, VAL_PERCENTILE, ID_SCEN, DES_SHOCK_FINALE, DES_PREPAYMENT, COD_RIPARTIZIONE)
+
+  filiali_estere <- .mapping_entity %>%
+    filter ( FLG_CAPOGRUPPO == 'N') %>%
+    select (COD_ENTITY) %>%
+    distinct() %>%
+    pull()
+
+  ECAP_1 <- ECAP %>%
+    group_by(ID_YEAR, COD_VALUTA, VAL_PERCENTILE, DES_PREPAYMENT) %>%
+    mutate(ECAP_00001 = ECAP[COD_ENTITY == "00001"]) %>%
+    filter(COD_ENTITY %in% c("00005", filiali_estere)) %>%
+    mutate(peso = ECAP/sum(ECAP),
+           ECAP = ECAP_00001*peso,
+           COD_RIPARTIZIONE = 1) %>%
+    ungroup() %>%
+    select(ID_YEAR, COD_VALUTA, COD_ENTITY, ECAP, VAL_PERCENTILE, ID_SCEN, DES_SHOCK_FINALE, DES_PREPAYMENT, COD_RIPARTIZIONE)
+
+  ECAP <- ECAP %>%
+    bind_rows(ECAP_1, ECAP_2)
+
+  valuta_tot <- ECAP %>%
+    group_by(ID_YEAR, COD_ENTITY, VAL_PERCENTILE, DES_SHOCK_FINALE, DES_PREPAYMENT, COD_RIPARTIZIONE) %>%
+    summarise(ECAP = sum(ECAP)) %>%
+    mutate(ID_SCEN = NA,
+           COD_VALUTA = "TOT") %>%
+    ungroup()
+
+  ECAP <- ECAP %>%
+    bind_rows(valuta_tot)
+
+  return(ECAP)
+}
---
+++ R/do_selezione_curve_ecap.R
@@ -0,0 +1,37 @@
+#' do_selezione_curve_ecap
+#' @description
+#' tba
+#' @param ecap a tibble object with 9 variables:
+#' * ID_YEAR dbl,
+#' * COD_VALUTA chr,
+#' * COD_ENTITY chr,
+#' * ECAP dbl,
+#' * VAL_PERCENTILE dbl,
+#' * ID_SCEN dbl,
+#' * DES_SHOCK_FINALE chr,
+#' * DES_PREPAYMENT chr,
+#' * COD_RIPARTIZIONE dbl.
+#' @param .curve_1y_interpol tibble with 6 variables:
+#' * COD_VALUTA chr,
+#' * ID_MESE_MAT dbl,
+#' * ID_YEAR dbl,
+#' * ID_SCEN dbl,
+#' * VAL_TASSO dbl.
+#' * DISCOUNT_FACTOR dbl.
+#' @return a list with 2 tibble, each with 5 variables:
+#' * COD_VALUTA chr,
+#' * ID_MESE_MAT dbl,
+#' * ID_YEAR dbl,
+#' * ID_SCEN dbl,
+#' * VAL_TASSO dbl.
+#' @export
+
+do_selezione_curve_ecap <- function(.ecap, .curve_1y_interpol){
+
+  .ecap %>%
+    filter(COD_RIPARTIZIONE == 0 & !(COD_VALUTA =="TOT") )%>%
+    left_join(.curve_1y_interpol, by = c( "ID_YEAR", "COD_VALUTA", "ID_SCEN")) %>%
+    select(ID_YEAR, COD_VALUTA, COD_ENTITY, VAL_PERCENTILE, ID_SCEN, DES_SHOCK_FINALE,
+           DES_PREPAYMENT, ID_MESE_MAT, VAL_TASSO)
+
+}
