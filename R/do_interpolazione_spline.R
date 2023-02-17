cb2c8c38309fd27770184d79338664f72eb9649e
emanuele-depaoli-sdg <emanuele.depaoli@sdggroup.com>
2023-02-17 12:56:50 +0000

    rename

--- R/do_interpolazione_spline.R
+++ R/do_interpolazione_spline.R
@@ -1,4 +1,4 @@
-#' do_interpolazione_curve
+#' do_interpolazione_spline
 #' @description
 #' Esegue interpolazione spline su VAL_TASSO, per ogni combinazione di ID_YEAR,
 #' COD_VALUTA e ID_SCEN.
@@ -8,6 +8,7 @@
 #' * ID_YEAR dbl,
 #' * ID_SCEN dbl,
 #' * VAL_TASSO dbl.
+#' @param .max_x
 #' @return tibble with 5 variables:
 #' * COD_VALUTA chr,
 #' * ID_MESE_MAT dbl,
@@ -15,13 +16,13 @@
 #' * ID_SCEN dbl,
 #' * VAL_TASSO dbl.
 #' @export
-do_interpolazione_spline <- function(.curve_1y) {
+do_interpolazione_spline <- function(.curve_1y, .max_x) {
 
   curve_1y_interpol <- curve_1y %>%
     group_by(ID_YEAR, COD_VALUTA, ID_SCEN) %>%
     # Returning more (or less) than 1 row per `summarise()` group was deprecated in dplyr 1.1.0.
     # use reframe()
-    reframe(.interpolazione_spline(.x = ID_MESE_MAT, .y = VAL_TASSO, .max_x = max_x),
+    reframe(.interpolazione_spline(.x = ID_MESE_MAT, .y = VAL_TASSO, .max_x = .max_x),
               .groups = "drop") %>%
     select(COD_VALUTA,
            ID_YEAR,
