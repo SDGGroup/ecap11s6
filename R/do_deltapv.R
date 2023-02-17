89699658b9d55447018db613c4a94c1ef081aaca
emanuele-depaoli-sdg <emanuele.depaoli@sdggroup.com>
2023-02-17 12:56:21 +0000

    move functions from utilis to do_deltapv and do_ecap

--- R/do_deltapv.R
+++ R/do_deltapv.R
@@ -112,3 +112,88 @@ do_deltapv <- function(.scenari_prep, .scenari_noprep, .notional, .notional_prep
 
   return(deltaPV)
 }
+
+#' .do_deltapv_gestionale
+#' @description
+#' Calcola DELTA_PV gestionale per ogni scenario contenuto in scenari, valuta ed
+#' entity contenuta in notional.
+#' @param .scenari tba
+#' @param .notional tba
+#' @param .curve_1y_interpol tba
+#' @return a tibble tba
+#' @export
+.do_deltapv_gestionale <- function(.scenari, .notional, .curve_1y_interpol){
+
+  scenari_notional <- .scenari %>%
+    inner_join(.notional,
+               by = c('COD_VALUTA' = 'COD_VALUTA_FINALE', 'DES_SHOCK_FINALE' = 'DES_SHOCK_FINALE')) %>%
+    select(ID_YEAR, COD_VALUTA, ID_SCEN, DES_SHOCK_FINALE, COD_ENTITY, ID_MESE_MAT, VAL_NOTIONAL)
+  # num = years*scenari*480*cod_entity
+
+  scenari_notional_curve <- scenari_notional %>%
+    left_join(.curve_1y_interpol, by = c("ID_YEAR", 'COD_VALUTA', 'ID_MESE_MAT', 'ID_SCEN')) %>%
+    select(ID_YEAR, COD_VALUTA, ID_SCEN, DES_SHOCK_FINALE, COD_ENTITY, ID_MESE_MAT, VAL_NOTIONAL, DISCOUNT_FACTOR)
+
+  discount_factor_0 <- .curve_1y_interpol %>%
+    filter (ID_SCEN == 0) %>%
+    select(ID_YEAR, COD_VALUTA,ID_MESE_MAT,DISCOUNT_FACTOR_0 = DISCOUNT_FACTOR)
+
+  scenari_notional_curve_df0 <- scenari_notional_curve %>%
+    left_join(discount_factor_0, by = c("ID_YEAR", 'COD_VALUTA','ID_MESE_MAT'))
+
+  deltaPV <- scenari_notional_curve_df0 %>%
+    mutate(PV_SIM = VAL_NOTIONAL * DISCOUNT_FACTOR,
+           PV_0 = VAL_NOTIONAL * DISCOUNT_FACTOR_0) %>%
+    group_by(ID_YEAR, COD_VALUTA , ID_SCEN , DES_SHOCK_FINALE,  COD_ENTITY ) %>%
+    summarise(PV_SIM = sum(PV_SIM, na.rm  = TRUE),
+              PV_0 = sum(PV_0),
+              .groups = 'drop') %>%
+    mutate(DELTA_PV = PV_SIM - PV_0) %>%
+    select(ID_YEAR, COD_VALUTA, ID_SCEN, DES_SHOCK_FINALE, COD_ENTITY, DELTA_PV)
+
+  return(deltaPV)
+}
+
+#' .do_deltapv_segnaletico
+#' @description
+#' Calcola DELTA_PV segnaletico per ogni scenario contenuto in scenari, valuta ed
+#' entity contenuta in notional.
+#' @param .scenari tba
+#' @param .notional tba
+#' @param .curve_1y_interpol tba
+#' @return a tibble tba
+#' @export
+do_deltapv_segnaletico <- function(.scenari, .notional, .notional_base, .curve_1y_interpol){
+
+  scenari_notional <- .scenari %>%
+    inner_join(.notional,
+               by = c('COD_VALUTA' = 'COD_VALUTA_FINALE', 'DES_SHOCK_FINALE' = 'DES_SHOCK_FINALE')) %>%
+    select(ID_YEAR, COD_VALUTA, ID_SCEN, DES_SHOCK_FINALE, COD_ENTITY, ID_MESE_MAT, VAL_NOTIONAL) %>%
+    left_join(.notional_base, by = c("COD_VALUTA" = "COD_VALUTA_FINALE", "COD_ENTITY", "ID_MESE_MAT")) %>%
+    select(ID_YEAR, COD_VALUTA, ID_SCEN, DES_SHOCK_FINALE, COD_ENTITY, ID_MESE_MAT,
+           VAL_NOTIONAL = VAL_NOTIONAL.x,
+           VAL_NOTIONAL_BASE = VAL_NOTIONAL.y)
+
+  scenari_notional_curve <- scenari_notional %>%
+    left_join(.curve_1y_interpol, by = c("ID_YEAR", 'COD_VALUTA', 'ID_MESE_MAT', 'ID_SCEN')) %>%
+    select(ID_YEAR, COD_VALUTA, ID_SCEN, DES_SHOCK_FINALE, COD_ENTITY, ID_MESE_MAT, VAL_NOTIONAL, VAL_NOTIONAL_BASE, DISCOUNT_FACTOR)
+
+  discount_factor_0 <- .curve_1y_interpol %>%
+    filter (ID_SCEN == 0) %>%
+    select(ID_YEAR, COD_VALUTA,ID_MESE_MAT,DISCOUNT_FACTOR_0 = DISCOUNT_FACTOR)
+
+  scenari_notional_curve_df0 <- scenari_notional_curve %>%
+    left_join(discount_factor_0, by = c("ID_YEAR", 'COD_VALUTA','ID_MESE_MAT'))
+
+  deltaPV <- scenari_notional_curve_df0 %>%
+    mutate(PV_SIM = VAL_NOTIONAL * DISCOUNT_FACTOR,
+           PV_0 = VAL_NOTIONAL_BASE * DISCOUNT_FACTOR_0) %>%
+    group_by(ID_YEAR, COD_VALUTA , ID_SCEN , DES_SHOCK_FINALE,  COD_ENTITY ) %>%
+    summarise(PV_SIM = sum(PV_SIM, na.rm  = TRUE),
+              PV_0 = sum(PV_0),
+              .groups = 'drop') %>%
+    mutate(DELTA_PV = PV_SIM - PV_0) %>%
+    select(ID_YEAR, COD_VALUTA, ID_SCEN, DES_SHOCK_FINALE, COD_ENTITY, DELTA_PV)
+
+  return(deltaPV)
+}
